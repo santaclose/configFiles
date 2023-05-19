bf89b88ffc6abf473e0335def9b6f2f38573c31f
Santiago <santidetaramonaf@gmail.com>
2023-05-19 17:10:52 +0200

    Create SetDefaultAppNoose.bat

---
+++ SetDefaultAppNoose.bat
@@ -0,0 +1,10 @@
+SetUserFTA .png Applications\noose.exe
+SetUserFTA .jpg Applications\noose.exe
+SetUserFTA .jpeg Applications\noose.exe
+SetUserFTA .bmp Applications\noose.exe
+SetUserFTA .tga Applications\noose.exe
+SetUserFTA .gif Applications\noose.exe
+SetUserFTA .psd Applications\noose.exe
+SetUserFTA .hdr Applications\noose.exe
+SetUserFTA .pic Applications\noose.exe
+pause
